9931fa494aa64d4c7f605ef23b011d1e0f773929
wang_chi <wang_chi@winning.com.cn>
2019-04-02 07:57:20 +0000

    finished matrix multiplication

--- homework1/problem2/matrix.cc
+++ homework1/problem2/matrix.cc
@@ -9,6 +9,7 @@
 //
 #include <mpi.h>
 #include <cassert>
+#include <vector>
 #include <random>
 #include <iostream>
 #include <boost/mpi/datatype.hpp>
@@ -18,18 +19,6 @@
 #include <boost/random/uniform_int_distribution.hpp>
 
 using namespace boost::numeric::ublas;
-
-void decompose_domain(int domain_size, int world_rank, int world_size,
-		int *subdomain_start, int *subdomain_size){
-	if( world_size > domain_size){
-		MPI_Abort(MPI_COMM_WORLD, 1);
-	}
-	*subdomain_start = domain_size / world_size * world_rank;
-	*subdomain_size = domain_size / world_size;
-	if( world_rank == world_size - 1){
-		*subdomain_size = domain_size % world_size;
-	}
-}
 
 template <typename T>
 matrix<T>* gen_matrix(size_t rows = 1024, size_t cols = 1024){
@@ -45,74 +34,108 @@ matrix<T>* gen_matrix(size_t rows = 1024, size_t cols = 1024){
 }
 
 template <typename T>
-matrix<T> matrix_mul_row_wise(matrix<T> m1, matrix<T> m2){
-  assert(m1.size2() == m2.size1());
+void vm_mul_row_wise(std::vector<T>* sub_vectors, matrix<T>* m, std::vector<T>* sub_out){
+  const int m_rows = m->size1();
+  const int m_cols = m->size2();
+  const int v_cols = m->size1();
+  const int v_rows = sub_vectors->size() / v_cols;
 
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  int world_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  
-  auto mpi_type = boost::mpi::get_mpi_datatype(m1(0,0));
-
-  // row of first matrix
-  int domain_size = m1.size1(); 
-  int subdomain_start, subdomain_size;
-  decompose_domain(domain_size, world_rank, world_size, &subdomain_start, &subdomain_size);
-  // std::cout << subdomain_start << subdomain_size << std::endl;
-
-  int recv_count =  subdomain_size * m1.size2();
-  T *sub_vectors = new T[recv_count];
-  assert(sub_vectors != NULL);
-
-  if( world_rank == 0){
-    // MPI_Scatter( m1.data().begin(), recv_count, mpi_type, sub_vectors, recv_count, mpi_type, 0, MPI_COMM_WORLD);
-    MPI_Bcast(m2.data().begin(), m2.size1() * m2.size2(), mpi_type, 0, MPI_COMM_WORLD);
+  for(int i=0; i < v_rows; i++){
+    for(int j=0; j < m_cols; j++){
+      T temp = 0;
+      for(int k=0; k < m_rows; k++){
+        temp += sub_vectors->at(i * v_cols + k) * m->at_element(k, j);
+      }
+      sub_out->at(i*v_cols + j) = temp;
+    }
   }
-
-  for(auto i = 0; i < m2.size1() * m2.size2(); i++){
-    // std::cout << sub_vectors[i] << std::endl;
-    std::cout << m2.data().begin()[i] << std::endl;
-    
-  }
-
-  delete sub_vectors;
-
-  return m1;
 }
 
-int main(int argc, char** argv) {
-  // if (argc != 3) {
-  //   fprintf(stderr, "Usage: avg num_elements_per_proc num_trials\n");
-  //   exit(1);
-  // }
+template <typename T>
+bool check_equality(matrix<T>* m1, matrix<T>* m2){
+  for(int i=0; i < m1->size1(); i++){
+    for(int j=0; j < m1->size2(); j++){
+      if(m1->at_element(i,j) != m2->at_element(i,j)){
+      	return false;
+      }
+    }
+  }
+  return true;
+}
 
-  // int num_elements_per_proc = atoi(argv[1]);
-  // int num_trials = atoi(argv[2]);
-
-
-  MPI_Init(NULL, NULL);
+void matrix_mul(const int rows=1024, const int cols=1024){
 
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  auto mpi_type = MPI_INT;
+  const int root = 0;
 
   matrix<int>* m1 = NULL;
   matrix<int>* m2 = NULL; 
-  if (world_rank == 0) {
-    m1 = gen_matrix<int>(5, 5);
-    m2 = gen_matrix<int>(5, 5);
-    std::cout << *m1 << std::endl;
-    std::cout << *m2 << std::endl;
+  matrix<int> m_origin_out; 
+  if (world_rank == root) {
+    m1 = gen_matrix<int>(rows, cols);
+    m2 = gen_matrix<int>(cols, rows);
+    m_origin_out = matrix<int>(prod(*m1, *m2));
+    MPI_Bcast(m2->data().begin(), rows * cols, mpi_type, root, MPI_COMM_WORLD);
+    MPI_Bcast(m_origin_out.data().begin(), rows * cols, mpi_type, root, MPI_COMM_WORLD);
+  }else{
+    m2 = new matrix<int>(rows, cols);
+    //m_origin_out = new matrix<int>(rows, cols);
+    m_origin_out = matrix<int>(rows, cols);
+    MPI_Bcast(m2->data().begin(), rows * cols, mpi_type, root, MPI_COMM_WORLD);
+    MPI_Bcast(m_origin_out.data().begin(), rows * rows, mpi_type, root, MPI_COMM_WORLD);
+  }
+  
+  if( rows % world_size != 0){
+    MPI_Abort(MPI_COMM_WORLD, -1);
+  }
+  int recv_count =  rows / world_size * cols;
+  
+  std::vector<int>* sub_vectors = new std::vector<int>(recv_count);
+  assert(sub_vectors != NULL);
+
+  if(world_rank == root){
+    MPI_Scatter( m1->data().begin(), recv_count, mpi_type, sub_vectors->data(), recv_count, mpi_type, root, MPI_COMM_WORLD);
+  } else{
+    MPI_Scatter( NULL, recv_count, mpi_type, sub_vectors->data(), recv_count, mpi_type, root, MPI_COMM_WORLD);
   }
 
-  matrix_mul_row_wise(*m1, *m2);
+  // v_rows * cols ,  cols * rows,  v_rows * rows
+  std::vector<int>* sub_out = new std::vector<int>(recv_count);
+  matrix<int>* m_out = new matrix<int>(rows, rows);
+  assert(m_out != NULL);
+
+  vm_mul_row_wise<int>(sub_vectors, m2, sub_out);
+
+  MPI_Allgather(sub_out->data(), recv_count, mpi_type, m_out->data().begin(), recv_count, mpi_type, MPI_COMM_WORLD); 
+
+  auto is_same = check_equality(m_out, &m_origin_out);
+  std::cout <<  "Rank:" << world_rank  << ",The two results are same:" << is_same << std::endl; 
   MPI_Barrier(MPI_COMM_WORLD);
-
-  if (world_rank == 0) {
+  //if(world_rank == 0){
+  //  std::cout << "Rank " << world_rank << " Out: " << *m_out <<  std::endl
+  //          << m_origin_out << std::endl;
+  //}
+  delete sub_out;
+  delete m_out;
+  delete m2;
+  if (world_rank == root) {
     delete m1;
-    delete m2;
   }
+}
+
+void matrix_pool(const int rows=1024, const int cols=1024){
+}
+
+int main(int argc, char** argv) {
+
+  MPI_Init(NULL, NULL);
+
+  matrix_mul(16, 16);
+
   MPI_Finalize();
 }
